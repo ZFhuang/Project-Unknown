54594d153380f7879f51f18233e5831753a8e4a1
ZFhuang <huangsnake@gmail.com>
2020-02-06 13:04:35 +0800

    SkipButton and halfly complete the scene0
    waiting to fix timeline playable track's bugs

---
+++ CompetitionProject/Assets/Scripts/Common/OpenScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class OpenScene : MonoBehaviour
+{
+    [SerializeField]
+    private Button skipButton;
+    //[SerializeField]
+    //private PlayableDirector director;
+
+    private Animation skipButtonShow;
+
+    public void nextScene()
+    {
+        //Load next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Start()
+    {
+        skipButtonShow = skipButton.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Click anywhere to active the skip button
+        if (Input.GetMouseButtonDown(0))
+        {
+            skipButton.gameObject.SetActive(true);
+            skipButtonShow.Rewind("SkipButtonShow");
+            skipButtonShow.Play("SkipButtonShow");
+
+        }
+
+        //Check if the animation is played
+        if (skipButton.IsActive() &&
+            Mathf.Abs(skipButtonShow["SkipButtonShow"].normalizedTime - 
+            skipButtonShow["SkipButtonShow"].length) <= 
+            skipButtonShow["SkipButtonShow"].length * 0.01)
+        {
+            skipButton.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ CompetitionProject/Assets/Scripts/Scene0/CaptionCon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CaptionCon : PlayableAsset
+{
+    public ExposedReference<Text> m_DialogContainer;
+    public string m_DialogStr;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        Playable playable= ScriptPlayable<PlayableTest>.Create(graph); ;
+        playable .m_DialogContainer = m_DialogContainer.Resolve(graph.GetResolver());
+        playable.GetBehaviour().m_DialogStr = m_DialogStr;
+        return playable;
+    }
+}
