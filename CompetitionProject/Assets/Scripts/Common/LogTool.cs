e3dbfb48a07cb389f0ea04bf89cb7b4cb80f6927
ZFhuang <huangsnake@gmail.com>
2020-02-06 18:16:41 +0800

    Complete the caption setting in Timeline
    here are some bugs in skip button

---
+++ CompetitionProject/Assets/Scripts/Common/LogTool.cs
@@ -0,0 +1,53 @@
+﻿/// From: 输出Log到手机屏幕 - 云影的文章 - 知乎
+///     https://zhuanlan.zhihu.com/p/34699914
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class ShowLogTool : MonoBehaviour
+{
+    public bool showFlag = true;
+    public int logCount = 100;
+    private string m_ShowLog = string.Empty;
+    private Queue<string> logQueue = new Queue<string>();
+
+    void Start()
+    {
+        Application.logMessageReceived += WriteUnityLog;
+    }
+
+
+    void WriteUnityLog(string log, string stackTrace, LogType type)
+    {
+        if (!showFlag) return;
+        WriteInLogQueue(log);
+    }
+
+    void WriteInLogQueue(string log)
+    {
+        logQueue.Enqueue(log);
+        while (logQueue.Count > logCount)
+        {
+            logQueue.Dequeue();
+        }
+        m_ShowLog = string.Empty;
+        foreach (string onelog in logQueue)
+        {
+            m_ShowLog = m_ShowLog + "\r\n" + onelog;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (showFlag)
+        {
+            GUIStyle style = new GUIStyle();
+            style.fontSize = 20;
+            style.normal.textColor = Color.red;
+
+            //GUI.color = Color.red;
+            GUI.Label(new Rect(0, 0, 1000, 1000), m_ShowLog, style);
+        }
+    }
+}
--- CompetitionProject/Assets/Scripts/Scene0/CaptionCon.cs
+++ CompetitionProject/Assets/Scripts/Scene0/CaptionCon.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
-using UnityEngine.UI;
 
+//Caption controller, this is the only type of script that can be added to Timeline
 [System.Serializable]
 public class CaptionCon : PlayableAsset
 {
-    public ExposedReference<Text> m_DialogContainer;
-    public string m_DialogStr;
+    //ExposedReference is used to convert objects to the real running script
+    public ExposedReference<TMPro.TextMeshProUGUI> dialogContainer;
+    public string dialogStr;
 
     // Factory method that generates a playable based on this asset
     public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
     {
-        Playable playable= ScriptPlayable<PlayableTest>.Create(graph); ;
-        playable .m_DialogContainer = m_DialogContainer.Resolve(graph.GetResolver());
-        playable.GetBehaviour().m_DialogStr = m_DialogStr;
-        return playable;
+        //The real running script class
+        Caption caption = new Caption();
+
+        //Set some values
+        caption.dialogStr = dialogStr;
+        //Set some objects
+        caption.dialogContainer = dialogContainer.Resolve(graph.GetResolver());
+
+        return ScriptPlayable<Caption>.Create(graph, caption);
     }
 }
