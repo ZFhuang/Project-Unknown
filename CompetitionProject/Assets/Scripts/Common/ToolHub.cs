7b8d2a72fade25bcb8ff0d1b1d9d21c5b2bf23e7
Zifeng <huangsnake@gmail.com>
2020-02-11 00:58:09 +0800

    ExHub's animation completed
    some layers still buging

---
+++ CompetitionProject/Assets/Scripts/Common/ToolHub.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolHub : MonoBehaviour
+{
+    bool isDown = false;
+    [SerializeField] Animator toolHubAni;
+    [SerializeField] Animator exHubAni;
+
+    public void action()
+    {
+        if (isDown)
+        {
+            Debug.Log("UP");
+            isDown = false;
+            toolHubAni.SetBool("isDown", isDown);
+            exHubAni.SetBool("isDown", isDown);
+        }
+        else
+        {
+            Debug.Log("DOWN");
+            isDown = true;
+            toolHubAni.SetBool("isDown", isDown);
+            exHubAni.SetBool("isDown", isDown);
+        }
+    }
+}
